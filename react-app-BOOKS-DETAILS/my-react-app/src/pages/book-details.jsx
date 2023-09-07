7489560388cde26017fcd2b9112e44ecd3ba322d
Ignacio <ignacio.damanes@gmail.com>
2023-09-07 15:12:02 -0300

    Maqueta book details V1

---
+++ react-app-BOOKS-DETAILS/my-react-app/src/pages/book-details.jsx
@@ -0,0 +1,16 @@
+import { NavBar } from '../components/nabvar';
+import { Footer } from '../components/footer';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle'
+
+export default function bookDetails() {
+    return(
+        <div>
+
+            <NavBar />
+
+            <Footer />
+
+        </div>
+    );
+}
